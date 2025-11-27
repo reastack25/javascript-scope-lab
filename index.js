8ffdbfe36557e84d3b19922dd127a666989fede8
reagan <221689087+reastack25@users.noreplygithub.com>
2025-11-27 23:21:04 +0300

    javascript code to meet test requirements

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+const burgers = ['Hamburger','Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+function addBurger (){
+    const newBurger = 'Flatburger';
+    burgers.push(newBurger);
+}
+
+// if statement with true condition
+if(true) {
+   const anotherNewBurger = 'Maple Bacon Burger';
+    burgers.push(anotherNewBurger);
+}
+
+
+// changeFeaturedDrink function
+function changeFeaturedDrink() {
+    featuredDrink = 'The JavaShake';
+}
